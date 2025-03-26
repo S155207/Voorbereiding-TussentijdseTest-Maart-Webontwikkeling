84cda03442f39566cc362aac22cd5f88323bb2c6
Török Gregory (s155207) <s155207@ap.be>
2025-03-26 23:47:14 +0000

    Complete exercise Tic Tac Toe from Labo 3

--- labos/labo3/tic-tac-toe/index.ts
+++ labos/labo3/tic-tac-toe/index.ts
@@ -21,6 +21,7 @@ while (!winnerFound) {
     if (checkWinner() !== "") {
         winnerFound = true;
         console.log(`Player ${checkWinner()} wins!`);
+        printBoard();
     };
 };
 
@@ -54,7 +55,7 @@ function checkWinner () {
     };
 
     // check diagonal winner
-    if (((board[0][0] === board[1][1] && board[0][0] === board[2][2]) || (board[0][2] === board[1][1] && board[0][0] === board[2][0])) && board[1][1] !== " ") {
+    if (((board[0][0] === board[1][1] && board[0][0] === board[2][2]) || (board[0][2] === board[1][1] && board[0][2] === board[2][0])) && board[1][1] !== " ") {
         return `${board[1][1]}`;
     };
 
