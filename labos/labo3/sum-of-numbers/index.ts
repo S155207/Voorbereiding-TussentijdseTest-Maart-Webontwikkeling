34c70cfa593c2edc4c47452d002cb4fdbd54fcab
Török Gregory (s155207) <s155207@ap.be>
2025-03-26 13:11:49 +0000

    Complete exercise Sum Of Numbers from Labo 3

--- labos/labo3/sum-of-numbers/index.ts
+++ labos/labo3/sum-of-numbers/index.ts
@@ -3,8 +3,9 @@ import * as readline from "readline-sync"
 
 const amountOfNumbers = readline.questionInt("Hoeveel getallen wil je optellen? ", {limitMessage: "I only like numbers!"});
 let numbers = [];
+let total = 0;
 for (let i = 0; i < amountOfNumbers; i++) {
     numbers[i] = readline.questionInt(`Geef getal ${i + 1} in: `, {limitMessage: "I only like numbers!"});
+    total += numbers[i];
 }
-const total = numbers.reduce((sum, num) => sum + num, 0);
 console.log(`De som van de getallen is ${total}`);
