1915545d7284879676c2a814fdba8e57fa7bef81
Török Gregory (s155207) <s155207@ap.be>
2025-03-27 15:17:45 +0000

    Complete exercise Short Notation from Labo 4

---
+++ labos/labo4/short-notation/index.ts
@@ -0,0 +1,14 @@
+// const printStuff = (amount: number, text: string):void => {
+//     console.log(`Hello ${text}, you are number ${amount}`);
+// }
+const printStuff = (amount: number, text: string) => console.log(`Hello ${text}, you are number ${amount}`);
+
+// const twoDArray = (element1: string, element2: string): string[] => {
+//     return [element1, element2];
+// }
+const twoDArray = (element1: string, element2: string) => [element1, element2];
+
+// const numberToString = (number: number): string => {
+//     return `${number}`;
+// }
+const numberToString = (number: number) => `${number}`;
